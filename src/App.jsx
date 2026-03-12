495c427ed344b7e5fb3db8fb5ff7a95031f0437f
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-12 05:56:55 -0500

    feat: Update App function

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,13 @@
+import { firstPicture, secondPicture, thirdPicture, fourthPicture } from './data';
+import ArtPicture from './components/ArtPicture';
+
 export function App() {
-  return (
-    <section>
-
-
-      <article>
-        <h5>
-          Ada King (1815–1852), Countess of Lovelace, Mathematician, Daughter of
-          Lord Byron
-        </h5>
-        <img
-          alt='first'
-          src='https://upload.wikimedia.org/wikipedia/commons/8/87/Ada_Lovelace.jpg'
-        />
-        <p>Author: Margaret Sarah Carpenter</p>
-      </article>
-
-
-      <article>
-        <h5>We Can Do It!</h5>
-        <img
-          alt='second'
-          src='https://upload.wikimedia.org/wikipedia/commons/1/12/We_Can_Do_It%21.jpg'
-        />
-        <p>Author: J. Howard Miller</p>
-      </article>
-
-
-      <article>
-        <h5>La joven de la perla</h5>
-        <img
-          alt='third'
-          src='https://upload.wikimedia.org/wikipedia/commons/thumb/d/d7/Meisje_met_de_parel.jpg/800px-Meisje_met_de_parel.jpg'
-        />
-        <p>Author: Johannes Vermeer</p>
-      </article>
-
-
-      <article>
-        <h5>Autoretrato con mono</h5>
-        <img
-          alt='fourth'
-          src='https://historia-arte.com/_/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpbSI6WyJcL2FydHdvcmtcL2ltYWdlRmlsZVwvNWYyNTMxM2UyZGUyZC5qcGciLCJyZXNpemUsODAwIl19.-dJRrycn7cE7szQihpGdLGy0woa-4K0SSluurohZlwE.jpg'
-        />
-        <p>Author: Frida Kahlo</p>
-      </article>
-
-
-    </section>
-  )
+    return (
+        <section>
+            <ArtPicture picture={firstPicture} />
+            <ArtPicture picture={secondPicture} />
+            <ArtPicture picture={thirdPicture} />
+            <ArtPicture picture={fourthPicture} />
+        </section>
+    );
 }
