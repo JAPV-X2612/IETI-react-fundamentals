9b341d6788f673c771986fc9e7e9bdf6cd91a622
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-17 23:44:38 -0500

    feat: Update files

--- src/components/CharacterSection.jsx
+++ src/components/CharacterSection.jsx
@@ -1,18 +1,18 @@
-import { currentTitles } from '../data/data';
-import { CharacterList } from './CharacterList';
-import { CustomButton } from './CustomButton';
-import { InputSearch } from './InputSearch';
-import { Paragraph } from './Paragraph';
-import { TitleSection } from './TitleSection';
+import { currentTitles } from "../data/data";
+import { CharacterList } from "./CharacterList";
+import { CustomButton } from "./CustomButton";
+import { InputSearch } from "./InputSearch";
+import { Paragraph } from "./Paragraph";
+import { TitleSection } from "./TitleSection";
 
 export const CharacterSection = () => (
-    <section>
-      <TitleSection>Characters</TitleSection>
-      <Paragraph>{`Showing ${currentTitles} titles`}</Paragraph>
-      <InputSearch/>
-      <CustomButton>Gender</CustomButton>
-      <CustomButton>Specie</CustomButton>
-      <CustomButton>Sort by:</CustomButton>
-      <CharacterList/>
-    </section>
+  <section>
+    <TitleSection>Characters</TitleSection>
+    <Paragraph>{`Showing ${currentTitles} titles`}</Paragraph>
+    <InputSearch />
+    <CustomButton>Gender</CustomButton>
+    <CustomButton>Specie</CustomButton>
+    <CustomButton>Sort by:</CustomButton>
+    <CharacterList />
+  </section>
 );
