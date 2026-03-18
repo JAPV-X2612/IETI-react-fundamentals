f909d8b5f83898fe53502f718ce5a92320c077b7
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-17 23:32:15 -0500

    format: Apply prettier and ESLints formats

--- src/components/CharacterCard.jsx
+++ src/components/CharacterCard.jsx
@@ -1,8 +1,8 @@
-export const CharacterCard = ({ name, imageURL }) => {
-	return (
-		<>
-			<img src={imageURL} alt='' />
-			<p onClick={() => alert(name)}>{name}</p>
-		</>
-	)
-}
+export const CharacterCard = ({name, imageURL}) => {
+  return (
+      <>
+        <img src={imageURL} alt=""/>
+        <p onClick={() => alert(name)}>{name}</p>
+      </>
+  );
+};
