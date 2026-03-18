c64af56c02695994f7f505275c4df209680dec0e
JAPV-X2612 <jesus.pinzon-v@mail.escuelaing.edu.co>
2026-03-17 22:46:33 -0500

    format: Apply prettier and ESLints formats

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { firstPicture, secondPicture, thirdPicture, fourthPicture } from './data';
-import ArtPicture from './components/ArtPicture';
+import {
+  firstPicture,
+  fourthPicture,
+  secondPicture,
+  thirdPicture,
+} from "./data";
+import ArtPicture from "./components/ArtPicture";
 
 export function App() {
-    return (
-        <section>
-            <ArtPicture picture={firstPicture} />
-            <ArtPicture picture={secondPicture} />
-            <ArtPicture picture={thirdPicture} />
-            <ArtPicture picture={fourthPicture} />
-        </section>
-    );
+  return (
+    <section>
+      <ArtPicture picture={firstPicture} />
+      <ArtPicture picture={secondPicture} />
+      <ArtPicture picture={thirdPicture} />
+      <ArtPicture picture={fourthPicture} />
+    </section>
+  );
 }
